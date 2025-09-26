b410875f7dfd8c478eb1f716e57bd8b9cffbae09
Vitor Hugo <vitor95340@gmail.com>
2025-09-26 00:37:21 -0300

    [feature]: semana 2 do desafio back-end alura terminada, adicionado a entidade categorias e tudo a respeito do mesmo

---
+++ src/main/java/com/desafio1/demo/categorias/CategoriasRepository.java
@@ -0,0 +1,6 @@
+package com.desafio1.demo.categorias;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriasRepository extends JpaRepository<Categorias, Long> {
+}
--- src/main/java/com/desafio1/demo/videos/Videos.java
+++ src/main/java/com/desafio1/demo/videos/Videos.java
@@ -1,7 +1,9 @@
 package com.desafio1.demo.videos;
 
+import com.desafio1.demo.categorias.Categorias;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
@@ -15,10 +17,20 @@ public class Videos {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
+    @NotBlank(message = "Titulo é obrigatório")
     private String titulo;
+
     @Column(columnDefinition = "text")
+    @NotBlank(message = "Descrição é obrigatório")
     private String descricao;
+
     @Column(nullable = false)
+    @NotBlank(message = "Url é obrigatório")
     private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_categoria", nullable = false)
+    private Categorias categoria;
 }
